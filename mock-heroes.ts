c244a2a8f5235db9d471897cc4b66300b482c2dc
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-02 15:22:24 -0700

    finished adding all the tasks

---
+++ mock-heroes.ts
@@ -0,0 +1,17 @@
+export const HEROES = [
+  {id: 11, name: 'Dr Bar'}
+  {id: 12, name: 'Dr Bar'}
+  {id: 13, name: 'Dr Bar'}
+  {id: 14, name: 'Dr Bar'}
+  {id: 15, name: 'Dr Bar'}
+  {id: 16, name: 'Dr Bar'}
+  {id: 17, name: 'Dr Bar'}
+  {id: 18, name: 'Dr Bar'}
+  {id: 19, name: 'Dr Bar'}
+  {id: 20, name: 'Dr Bar'}
+  {id: 21, name: 'Dr Bar'}
+  {id: 22, name: 'Dr Bar'}
+  {id: 23, name: 'Dr Bar'}
+  {id: 24, name: 'Dr Bar'}
+  {id: 25, name: 'Dr Bar'}
+]
